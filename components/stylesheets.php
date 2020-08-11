739108f85adc735c8543f35e9fa5c1876b744e34
Gourang Abani <gourangabani@gmail.com>
2020-08-11 22:16:39 +0530

    modularised sylesheets

---
+++ components/stylesheets.php
@@ -0,0 +1,6 @@
+<!-- google fonts-->
+<link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css" />
+<link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
+<link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+<!-- core theme CSS (includes bootstrap) -->
+<link href="css/styles.css" rel="stylesheet" />
--- index.php
+++ index.php
@@ -45,12 +45,9 @@ include 'data/sponsors.php';
     <link rel="apple-touch-icon" href="apple-touch-icon.png">
     <link rel="apple-touch-icon" sizes="72x72" href="apple-touch-icon-72x72.png">
     <link rel="apple-touch-icon" sizes="114x114" href="apple-touch-icon-114x114.png">
-    <!-- google fonts-->
-    <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css" />
-    <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
-    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
-    <!-- core theme CSS (includes bootstrap) -->
-    <link href="css/styles.css" rel="stylesheet" />
+    <?php
+    include 'components/stylesheets.php';
+    ?>
 </head>
 
 <body id="page-top">
