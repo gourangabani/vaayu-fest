07196a02f26a4f09ab76aa67a03257883e35b72f
Gourang Abani <gourangabani@gmail.com>
2020-08-11 22:32:36 +0530

    dump created

--- components/modals.php
+++ components/modals.php
@@ -1,27 +1,28 @@
-<!-- modal -->
-    <!-- <div class="portfolio-modal modal fade" id="hod-marketing-modal" tabindex="-1" role="dialog" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="close-modal" data-dismiss="modal">
-                    <img src="assets/img/close-icon.svg" alt="Close Modal" />
-                </div>
-                <div class="container">
-                    <div class="row justify-content-center">
-                        <div class="col-lg-12">
-                            <div class="modal-body">
-                                <h2 class="text-uppercase pb-5">
-                                    FINANCIER
-                                </h2>
-                                <p class="item-intro text-muted" id="purpose-modal-text">
-                                    
-                                </p>
-                                <button class="btn btn-custom-primary" data-dismiss="modal" type="button">
-                                    Close
-                                </button>
-                            </div>
+<!--
+<div class="portfolio-modal modal fade" id="hod-marketing-modal" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="close-modal" data-dismiss="modal">
+                <img src="assets/img/close-icon.svg" alt="Close Modal" />
+            </div>
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-12">
+                        <div class="modal-body">
+                            <h2 class="text-uppercase pb-5">
+                                FINANCIER
+                            </h2>
+                            <p class="item-intro text-muted" id="purpose-modal-text">
+                                
+                            </p>
+                            <button class="btn btn-custom-primary" data-dismiss="modal" type="button">
+                                Close
+                            </button>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div> -->
+    </div>
+</div>
+-->
--- index.php
+++ index.php
@@ -85,32 +85,6 @@ include 'data/sponsors.php';
                 <hr>
             </div>
         </section>
-        <!-- <section id="about">
-            <div id="carouselExampleIndicators" class="carousel slide vh-100" data-ride="carousel">
-                <ol class="carousel-indicators">
-                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                </ol>
-                <div class="carousel-inner vh-100 img-gradient">
-                    <div class="carousel-item active vh-100 vw-100">
-                        <img class="vh-100 vw-100" src="https://www.vaayufest.org/images/background-19.jpg"
-                            alt="First slide">
-                        <div class="carousel-caption text-center text-white">
-                            <h2>600 Souls. 4 Days. 1 Dream.</h2>
-                        </div>
-                    </div>
-                </div>
-                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
-            </div>
-        </section> -->
         <!-- about us -->
         <section class="separate-section container" id="about-us">
             <div class="separate-section-content">
@@ -506,7 +480,7 @@ include 'data/sponsors.php';
     }
     ?>
     <?php
-    include 'components/modals.php';
+    // include 'components/modals.php';
     ?>
     <div class="portfolio-modal modal fade" id="film-making-workshop-modal" tabindex="-1" role="dialog" aria-hidden="true">
         <div class="modal-dialog">
