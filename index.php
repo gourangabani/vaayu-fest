4480103e340e9f9937964fe80f2db990f8fb40e9
Gourang Abani <gourangabani@gmail.com>
2020-08-11 22:13:44 +0530

    modularised scripts

--- index.php
+++ index.php
@@ -597,15 +597,9 @@ include 'data/sponsors.php';
             </div>
         </div>
     </div>
-    <!-- bootstrap core js -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
-    <!-- third party plugin js -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
-    <!-- gsap -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.4.1/gsap.min.js"></script>
-    <!-- core theme js -->
-    <script src="js/scripts.js"></script>
+    <?php
+    include 'components/scripts.php';
+    ?>
 </body>
 
 </html>
