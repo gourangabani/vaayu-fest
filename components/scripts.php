770809bd2ee63eacda5ef5c0b4a333da29c3c73c
Gourang Abani <gourangabani@gmail.com>
2020-08-13 00:57:11 +0530

    modularised scripts

--- components/scripts.php
+++ components/scripts.php
@@ -3,9 +3,31 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
 <!-- third party plugin js -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
-<!-- gsap -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.4.1/gsap.min.js"></script>
-<!-- core theme js -->
-<script src="js/scripts.js"></script>
 <!-- font awesome icons (free version) -->
 <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
+<!-- gsap -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.4.1/gsap.min.js"></script>
+<!-- custom js -->
+<script src="js/custom.js"></script>
+<!-- vendor js -->
+<script src="js/vendor.js"></script>
+<!-- not to be cached -->
+<script>
+    function Modal() {
+        var custom_modal = $('#film-making-workshop-modal');
+        setTimeout(function() {
+            custom_modal.modal();
+        }, 10000);
+    }
+
+    function setModal() {
+        if (!sessionStorage.getItem('doNotShowModal')) {
+            sessionStorage.setItem('doNotShowModal', true);
+            Modal();
+        } else {
+            $('.film-making-workshop-modal').hide();
+        }
+    };
+
+    setModal();
+</script>
--- js/vendor.js
+++ js/vendor.js
@@ -4,57 +4,6 @@
  * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
  */
 
-function Preloader() {
-    var preloader = $('.preloader');
-    preloader.delay(7500).fadeOut(500);
-}
-
-function setPreloader() {
-    if (!sessionStorage.getItem('doNotShow')) {
-        sessionStorage.setItem('doNotShow', true);
-        Preloader();
-    } else {
-        $('.preloader').hide();
-    }
-};
-
-function Modal() {
-    var custom_modal = $('#film-making-workshop-modal');
-    setTimeout(function () {
-        custom_modal.modal();
-    }, 10000);
-}
-
-function setModal() {
-    if (!sessionStorage.getItem('doNotShowModal')) {
-        sessionStorage.setItem('doNotShowModal', true);
-        Modal();
-    } else {
-        $('.film-making-workshop-modal').hide();
-    }
-};
-
-// setPreloader();
-setModal();
-
-// let tl = gsap.timeline({
-//     delay: 0.5
-// });
-
-// tl.from('.power-4', {
-//         y: -100,
-//         opacity: 0,
-//         ease: 'slow',
-//         duration: 1.5
-//     }, 0.7)
-//     .from('.power-2', {
-//         // x: -50,
-//         y: 50,
-//         opacity: 0,
-//         ease: 'power2',
-//         duration: 2
-//     }, 1);
-
 (function ($) {
     "use strict"; // Start of use strict
 
@@ -116,15 +65,5 @@ setModal();
 
         // if element is between window's top and bottom edges
         return elementTopEdge + offset <= windowBottomEdge;
-    }
-
-    if (onScreen($("#contact-us"))) {
-        gsap.from(".power-2", {
-            duration: 0.5,
-            opacity: 0,
-            y: -100,
-            stagger: 0.1,
-            ease: "back.in"
-        });
     }
 })(jQuery); // End of use strict
