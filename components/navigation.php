581ba68edfdc51063ccf01129d510f0d63fd9dcf
Gourang Abani <gourangabani@gmail.com>
2020-08-16 22:05:10 +0530

    updated relativity

--- components/navigation.php
+++ components/navigation.php
@@ -6,7 +6,7 @@ require 'developmentMachineLogic.php';
 <nav class="navbar navbar-expand-lg navbar-dark bg-custom-primary fixed-top" id="sideNav">
     <a class="navbar-brand js-scroll-trigger" href="#page-top">
         <span class="d-block d-lg-none">Vaayu Fest</span>
-        <span class="d-none d-lg-block"><img class="img-fluid mx-auto mb-2" src="<?php $_SERVER['DOCUMENT_ROOT'] . '/' . $base; ?>/assets/img/vaayu-20-logo-transparent.png" alt="Logo" /></span>
+        <span class="d-none d-lg-block"><img class="img-fluid mx-auto mb-2" src="<?php $_SERVER['DOCUMENT_ROOT'] . '/' . $base; ?>assets/img/vaayu-20-logo-transparent.png" alt="Logo" /></span>
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
     <?php
@@ -17,7 +17,7 @@ require 'developmentMachineLogic.php';
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link" href="' . $_SERVER['DOCUMENT_ROOT'] . '/' . $base . '/index.php">
+                    <a class="nav-link" href="' . $_SERVER['DOCUMENT_ROOT'] . '/' . $base . 'index.php">
                         Home
                     </a>
                 </li>
