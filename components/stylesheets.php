a11d170ccf952b1ab48cc63a7d67b6e43a4a17de
Gourang Abani <gourangabani@gmail.com>
2020-08-17 01:29:57 +0530

    code quality

--- components/stylesheets.php
+++ components/stylesheets.php
@@ -5,6 +5,6 @@ require 'developmentMachineLogic.php';
 <!-- google fonts-->
 <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css" />
 <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
-<link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet" />
 <!-- core theme CSS (includes bootstrap) -->
 <link href="<?php echo $base; ?>css/styles.css" rel="stylesheet" />
