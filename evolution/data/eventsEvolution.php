e463411a47bd1d3f035e6b5f0196a59dc6784fdf
Gourang Abani <gourangabani@gmail.com>
2020-08-18 14:28:15 +0530

    data updated

--- evolution/data/eventsEvolution.php
+++ evolution/data/eventsEvolution.php
@@ -5,9 +5,9 @@ $marketingManiac = new EventEvolution(
     'marketing-maniac',
     'Maketing Event',
     'Marketing Maniac',
-    '',
+    'Know the art of selling? Ready to do it in a more challenging environment with the opportunity to bag an internship in one of the top marketing firms in India. Marketing Maniac is an event which will test your marketing skills and give you an experience of how marketing is done in the real world.',
     'marketing-maniac.jpg',
-    eventType::individual,
+    eventType::individual
 );
 
 $pixels = new EventEvolution(
@@ -15,7 +15,7 @@ $pixels = new EventEvolution(
     'pixels',
     'Graphic Design Event',
     'Pixels',
-    '',
+    'The mystery makers behind the eye-catching magazine posters, Graphic Designers have been the need of the hour not just for magazines but many other businesses. We are here with a platform for these skilled designers to compete in a more challenging environment with an incredible opportunity to bag a graphic designing internship and get to learn from and work with the best in the business. So join us at Pixels and show us what you can design.',
     'pixels.jpg',
     eventType::individual
 );
@@ -25,7 +25,7 @@ $vlogInn = new EventEvolution(
     'vlog-inn',
     'Vlogging Event',
     'Vlog-Inn',
-    '',
+    'With eyes like a shutter and mind like a lens, VlogInn a day of your LockInn!! You are the craftsman of your video. As you go through the daily routine, video blog a part of your day, include tasks and twists, add your touch to it and be as creative as you can be. An event for all the vloggers out there as we give you a chance to not miss out on vlogging even if you’re locked in. The participants have to include a set of tasks that would be sent to them.',
     'vlog-inn.jpg',
     eventType::individual
 );
@@ -35,7 +35,7 @@ $beYou = new EventEvolution(
     'be-you',
     'Personality Contest',
     'BeYou',
-    '',
+    'You are often told to be like someone or do what others do but why be anyone else, when you are the best version of yourself?? YOU are your own theme and you just have to Be You and Be creative to get through. Walk the ramp, be confident and do what you are best at doing as this personality contest aims to bring the best in you. This lockdown, "Don\'t look around, Look within - As you Celebrate You!!"',
     'be-you.jpg',
     eventType::individual
 );
@@ -45,7 +45,7 @@ $escapeIn = new EventEvolution(
     'escape-room',
     'Escape Room',
     'Escape In',
-    '',
+    'All of us are on the lookout for ways to escape this lockdown, but can you escape this virtual room? With this event, we bring to you a virtual escape room that will break all boredom boundaries in this lockdown. Experience this with friends as you race against time and savor victory when you reach your destination. With this perfect blend, one thing is for sure- You Can’t Escape the Thrill!!',
     'escape-in.jpg',
     eventType::team
 );
@@ -55,7 +55,7 @@ $stepInStyle = new EventEvolution(
     'step-in-style',
     'Dance Event',
     'Step In Style',
-    '',
+    'Do it big, do it right and do it with style. Display your brilliance, dance your heart out and emote using one dance form you love the most. Flaunt your moves and unleash your solo powers! Take more chances, dance more dances.',
     'step-in-style.jpg',
     eventType::individual
 );
@@ -65,7 +65,7 @@ $bollywoodzilla = new EventEvolution(
     'bollywoodzilla',
     'Music Event',
     'Bollywoodzilla',
-    '',
+    'Gear up and get your vocal cords ready for Vaayu\'s first online Bollywood solo singing competition! Show off your mashups, harmonies & medleys and leave an impression with your voice! Make your voice your wand and cast your spell singing from your heart, winning over everyone.',
     'bollywoodzilla.jpg',
     eventType::individual
 );
@@ -75,7 +75,7 @@ $hustleAndMuscle = new EventEvolution(
     'hustle-and-muscle',
     'Fitness Event',
     'Hustle & Muscle',
-    '',
+    'One of the most stimulating and testing event at Vaayu 20, this Fitness event allows you to feature your quality through home exercise with no apparatus. Event which revolves around continuing to remain fit regardless, when everyone is stuck at home and endeavouring to make the most out of it, squash hard!',
     'hustle-and-muscle.jpg',
     eventType::individual
 );
@@ -85,7 +85,7 @@ $versusVersusVersus = new EventEvolution(
     'versus-versus-versus',
     'Poetry Event',
     'Versus Versus Versus',
-    '',
+    'A poet’s ideas and emotions are often displayed by the works they create turning sometimes mundane topics into pieces of serenity. The idea behind a poem is to tell a story but in its own unique way. So for this event participants will test their skills in three innovative rounds made specifically to extract the story behind every poem. But the journey will not be as simple as it seems.',
     'versus-versus-versus.jpg',
     eventType::individual
 );
@@ -95,7 +95,7 @@ $writeCameraAction = new EventEvolution(
     'write-camera-action',
     'Monologue Event',
     'Write, Camera, Action',
-    '',
+    'The job of an actor is to effortlessly bring to life the meaning and idea behind the words written by the writer. To communicate and make the audience see the intricacies defined by the writer in the script is what an actor strives to achieve in a monologue. By making participants get in touch with their emotions we assure you your creativity and adaptability will be questioned, so let’s see who can rise to the occasion.',
     'write-camera-action.jpg',
     eventType::team
 );
@@ -105,7 +105,7 @@ $stonks = new EventEvolution(
     'stonks',
     'Virtual Stock Market Event',
     'Stonks!',
-    '',
+    'Your dream to excel in the dynamic world of stock market will now turn into reality! We present you with an exciting platform to create whip-smart strategies and trade stocks using a virtual portfolio on a real-time trading platform. All you need to do is strategize and trade in the stocks of NIFTY 500 for a client. Both, the profit that you earn and the strategy that you use will help you pave your way towards victory. So, sharpen your wits and tame your beasts because the unpredictable journey of the rise and fall of stock market is about to begin.',
     'stonks.jpg',
     eventType::individual
 );
@@ -115,7 +115,7 @@ $crisimulation = new EventEvolution(
     'crisimulation',
     'Crisis Simulation Event',
     'Crisimulation',
-    '',
+    'In the midst of the crisis that has taken over the world, the ability to deal with one has become increasingly important. This crisis will force us to see what the world needs before a crisis of this measure overtakes us again. The event begins with the obvious making of a plan by understanding the crisis, how it affects you and what you can do. With the combined intellect of the team, you can ensure to leave no stone unturned. This is followed by the validation of the plan so as to understand how well it can withstand any complications. Navigate the crisis to reach the optimum solution.',
     'crisimulation.jpg',
     eventType::team
 );
@@ -125,7 +125,7 @@ $disenoEvento = new EventEvolution(
     'diseno-evento',
     'Object Painting',
     'Diseno Evento',
-    '',
+    'Rack your brains and turn the object into something that one can not imagine! Paint, design and be as creative as one can fathom. Transform your object into one beautiful visual and create a picture that speaks for itself The whole purpose of the event is to let the participants showcase their talents in the form of art, be as creative and imaginative as possible and give the best outcome. The participants will be given an object to design upon on the given theme and they need to give their best to reach the top!',
     'diseno-evento.jpg',
     eventType::individual
 );
