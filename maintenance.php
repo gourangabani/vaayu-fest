a40af0fb372d75dbb6766a3e32eb29002cc2cf6b
Gourang Abani <gourangabani@gmail.com>
2020-08-18 14:16:43 +0530

    created an under maintenance page

---
+++ maintenance.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php
+    include 'components/meta.php';
+    ?>
+    <meta name="description" content="What was conceived out of pure passion for empowering the youth of our nation, has now transformed into one of the fastest growing cultural festivals of India. Boasting an annual footfall of about 35000, Vaayu is so much more than what meets the eye: an assemblage of people from throughout the country, an opportunity to jump start your career under the guidance of the most renowned entities, a median that promotes youth empowerment through every facet of its activities, but most importantly, it is a place where everlasting bonds are forged." />
+    <meta property="og:title" content="Vaayu '20" />
+    <meta property="og:description" content="Site under maintenance." />
+    <title>
+        Vaayu Fest - Under Maintenance
+    </title>
+    <?php
+    include 'components/stylesheets.php';
+    ?>
+</head>
+
+<body id="page-top">
+    <?php
+    include 'components/navigation.php';
+    ?>
+    <div class="container-fluid">
+        <!-- masthead -->
+        <section class="separate-section masthead 100vh" id="masthead">
+            <div class="separate-section-content container">
+                <h1 class="typewriter">
+                    Vaayu '20
+                </h1>
+                <h2 class="typewriter">
+                    We're under maintenance right now. Will be back up soon.
+                </h2>
+                <hr>
+            </div>
+        </section>
+    </div>
+</body>
+
+</html>
