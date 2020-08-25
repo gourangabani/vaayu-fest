bed262b3dd8d138f905a9fb6d5eb8c7f65b32740
harshit2283 <hpparikh16@gmail.com>
2020-08-25 22:05:39 +0530

    Update script.js

--- alpha/script.js
+++ alpha/script.js
@@ -1,9 +1,2 @@
-function switchStyle() {
-  if (document.getElementById('styleSwitch').checked) {
-    document.getElementById('gallery').classList.add("custom");
-    document.getElementById('exampleModal').classList.add("custom");
-  } else {
-    document.getElementById('gallery').classList.remove("custom");
-    document.getElementById('exampleModal').classList.remove("custom");
-  }
-}
+const observer = lozad();
+observer.observe();
