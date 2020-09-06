ae129d1ff31bf445090e2127d6719410102df842
Gourang Abani <gourangabani@gmail.com>
2020-09-06 16:52:24 +0530

    redirection link

---
+++ freshers-night.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php
+    include 'components/meta.php';
+    ?>
+    <meta name="description" content="What was conceived out of pure passion for empowering the youth of our nation, has now transformed into one of the fastest growing cultural festivals of India. Boasting an annual footfall of about 35000, Vaayu is so much more than what meets the eye: an assemblage of people from throughout the country, an opportunity to jump start your career under the guidance of the most renowned entities, a median that promotes youth empowerment through every facet of its activities, but most importantly, it is a place where everlasting bonds are forged." />
+    <meta property="og:title" content="Vaayu '20 - Freshers' Night" />
+    <meta property="og:description" content="Turn on in." />
+    <meta http-equiv="refresh" content="3;URL='https://www.youtube.com/watch?v=2x2XQhBKh6Q&feature=youtu.be'" />
+    <title>
+        Vaayu Fest - Freshers' Night
+    </title>
+    <?php
+    include 'components/stylesheets.php';
+    ?>
+</head>
+
+<body id="page-top">
+    <?php
+    // include 'components/preloader.php';
+    include 'components/navigation.php';
+    ?>
+    <div class="container-fluid">
+        <!-- redirect -->
+        <section class="separate-section container bg-gradient" id="redirect">
+            <div class="separate-section-content">
+                <div class="p-5 bg-white rounded shadow mb-5 text-center">
+                    <h2 class="mb-5">
+                        The Freshers' Night
+                    </h2>
+                    <h3 class="mb-5">
+                    </h3>
+                    <div class="row">
+                        <div class="col-md-4"></div>
+                        <div class="col-md-4">
+                            <img src="media/images/events/freshers-night.jpg" alt="Freshers' Night Poster" width="100%" />
+                        </div>
+                        <div class="col-md-4"></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5 mt-5">
+                        <div class="flex-grow-1">
+                            <h3>
+                                Get ready! You'll be redirected in 3 seconds. If not, please click <a href="https://www.youtube.com/watch?v=2x2XQhBKh6Q&feature=youtu.be" target="_blank"><span style="color: #e43f5a; font-weight: bold;">here</span></a>.
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</body>
+
+</html>
+
+<?php
+include 'components/notes.php';
+?>
