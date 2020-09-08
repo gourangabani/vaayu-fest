890fae02c93f9ca3b10e29762288dfdd334b95d3
Gourang Abani <gourangabani@gmail.com>
2020-09-09 00:49:08 +0530

    removed v2

--- v2/components/developmentMachine.php
+++
@@ -1,7 +0,0 @@
-<?php
-$base = '';
-if ($_SERVER['SERVER_NAME'] == 'localhost') {
-    $base = 'http://' . $_SERVER['SERVER_NAME'] . ':8888/candid-expressions/';
-} else {
-    $base = 'https://' . $_SERVER['SERVER_NAME'] . '/';
-}
