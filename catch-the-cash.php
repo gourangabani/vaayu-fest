3d7b059d00cdb6448a4dfcd2e2350227cd1caa20
Gourang Abani <gourangabani@gmail.com>
2020-10-27 19:12:08 +0530

    event redirection

---
+++ catch-the-cash.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include_once('./components/meta.php') ?>
+    <meta name="description" content="VAAYU: a metaphor of people, a carrier of hope, a treasure of memories, a haven for magical moments, a dream lived by many. It's a rising: invincible, powerful, mystical." />
+    <meta property="og:title" content="Vaayu '20 - Get Empowered!" />
+    <meta property="og:description" content="VAAYU: a metaphor of people, a carrier of hope, a treasure of memories, a haven for magical moments, a dream lived by many. It's a rising: invincible, powerful, mystical." />
+    <title>
+        Vaayu Fest - Catch The Cash
+    </title>
+    <?php include_once('./components/stylesheets.php') ?>
+</head>
+
+<body id="page-top">
+    <?php include_once './components/navigation.php'; ?>
+    <!-- catch the cash - registrations -->
+    <section id="sponsorsSection" class="customSection">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-1 col-md-1 col-sm-12">
+                    <h2 class="customSectionTitle">
+                        CATCHTHECASH
+                    </h2>
+                </div>
+                <div class="col-lg-11 col-md-11 col-sm-12">
+                    <div class="typeform-widget" data-url="https://form.typeform.com/to/O71QfP49" style="width: 100%; height: 500px;"></div>
+                    <script>
+                        (function() {
+                            var qs, js, q, s, d = document,
+                                gi = d.getElementById,
+                                ce = d.createElement,
+                                gt = d.getElementsByTagName,
+                                id = "typef_orm",
+                                b = "https://embed.typeform.com/";
+                            if (!gi.call(d, id)) {
+                                js = ce.call(d, "script");
+                                js.id = id;
+                                js.src = b + "embed.js";
+                                q = gt.call(d, "script")[0];
+                                q.parentNode.insertBefore(js, q)
+                            }
+                        })()
+                    </script>
+                    <div style="font-family: Sans-Serif;font-size: 12px;color: #999;opacity: 0.5; padding-top: 5px;"> powered by <a href="https://admin.typeform.com/signup?utm_campaign=O71QfP49&utm_source=typeform.com-01EK0DNDMJYN7M3TKP56N2NQA8-professional&utm_medium=typeform&utm_content=typeform-embedded-poweredbytypeform&utm_term=EN" style="color: #999" target="_blank">Typeform</a> </div>
+                </div>
+            </div>
+            <div class="row">
+            </div>
+        </div>
+    </section>
+    <?php include_once './components/footer.php'; ?>
+    <?php include_once './components/scripts.php'; ?>
+    <?php include_once './components/modals.php'; ?>
+</body>
+
+</html>
---
+++ redirect.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include_once('./components/meta.php') ?>
+    <meta name="description" content="VAAYU: a metaphor of people, a carrier of hope, a treasure of memories, a haven for magical moments, a dream lived by many. It's a rising: invincible, powerful, mystical." />
+    <meta property="og:title" content="Vaayu '20 - Get Empowered!" />
+    <meta property="og:description" content="VAAYU: a metaphor of people, a carrier of hope, a treasure of memories, a haven for magical moments, a dream lived by many. It's a rising: invincible, powerful, mystical." />
+    <meta http-equiv="refresh" content="3;url=https://youtu.be/od18F9nLoxg" />
+    <title>
+        Vaayu Fest - Redirect
+    </title>
+    <?php include_once('./components/stylesheets.php') ?>
+</head>
+
+<body id="page-top">
+    <?php include_once './components/navigation.php'; ?>
+    <!-- masthead -->
+    <header id="homeMasthead" class="customHeader text-center text-white">
+        <div id="homeMastheadOverlay" class="overlay"></div>
+        <video playsinline="playsinline" autoplay="autoplay" muted="muted">
+            <source src="media/vaayu-reveal-2.mp4" type="video/mp4">
+        </video>
+        <div class="container h-100">
+            <div class="d-flex h-100 text-center align-items-end">
+                <div class="w-100 text-white">
+                    <h6 class="mb-5">
+                        Redirecting... click <a href="https://www.youtube.com/watch?v=od18F9nLoxg&feature=youtu.be">here</a> if not redirected in 3 seconds.
+                    </h6>
+                </div>
+            </div>
+        </div>
+    </header>
+    <?php include_once './components/footer.php'; ?>
+    <?php include_once './components/scripts.php'; ?>
+    <?php include_once './components/modals.php'; ?>
+</body>
+
+</html>
