c957c754d4fa44e6901a533e975a3a82c28eb0c7
Gourang Abani <gourangabani@gmail.com>
2020-10-27 19:11:44 +0530

    init firebase

---
+++ assets/js/firebase/configuration.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyBNKmIjjLeoz35PFX-evYvdT-A9l1TDWN0",
+    authDomain: "vaayufest-v2.firebaseapp.com",
+    databaseURL: "https://vaayufest-v2.firebaseio.com",
+    projectId: "vaayufest-v2",
+    storageBucket: "vaayufest-v2.appspot.com",
+    messagingSenderId: "961698266745",
+    appId: "1:961698266745:web:abc9542613581a42151ea8",
+    measurementId: "G-Y93XWJZYZV"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
