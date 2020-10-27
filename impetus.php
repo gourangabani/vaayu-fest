4ce31d8c5f592c6586d79e6ee19df39a94ec0a3f
Gourang Abani <gourangabani@gmail.com>
2020-10-27 19:11:53 +0530

    magazine redirection

---
+++ impetus.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include_once('./components/meta.php') ?>
+    <meta name="description" content="VAAYU: a metaphor of people, a carrier of hope, a treasure of memories, a haven for magical moments, a dream lived by many. It's a rising: invincible, powerful, mystical." />
+    <meta property="og:title" content="Vaayu '20 - Get Empowered!" />
+    <meta property="og:description" content="VAAYU: a metaphor of people, a carrier of hope, a treasure of memories, a haven for magical moments, a dream lived by many. It's a rising: invincible, powerful, mystical." />
+    <title>
+        Vaayu Fest - Impetus
+    </title>
+    <?php include_once('./components/stylesheets.php') ?>
+    <meta http-equiv="refresh" content="3; url=https://vaayufest.org/media/impetus/september-20-1.pdf" />
+</head>
+
+<body id="page-top">
+    <?php include_once './components/navigation.php'; ?>
+    <!-- catch the cash - registrations -->
+    <section id="sponsorsSection" class="customSection pt-5 pb-5">
+        <div class="container">
+            <div class="row pt-5 pb-5">
+                <div class="col-lg-1 col-md-1 col-sm-12">
+                    <h2 class="customSectionTitle">
+                        IMPETUS
+                    </h2>
+                </div>
+                <div class="col-lg-11 col-md-11 col-sm-12">
+                    <h1 class="text-center">
+                        Loading September's Issue.
+                    </h1>
+                    <p class="text-center">
+                        Please check your downloads in case it hasn't opened yet. Click <a href="https://vaayufest.org/media/impetus/september-20-1.pdf">here</a> to open the file directly.
+                    </p>
+                </div>
+            </div>
+            <div class="row">
+            </div>
+        </div>
+    </section>
+    <?php include_once './components/footer.php'; ?>
+    <?php include_once './components/scripts.php'; ?>
+    <?php include_once './components/modals.php'; ?>
+</body>
+
+</html>
