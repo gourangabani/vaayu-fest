4154eaf025ffd9a50f3027c3986e20b734dbe7a1
Gourang Abani <gourangabani@gmail.com>
2020-11-25 17:30:48 +0530

    added oct issue

---
+++ happenings.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include_once('./components/meta.php') ?>
+    <meta name="description" content="VAAYU: a metaphor of people, a carrier of hope, a treasure of memories, a haven for magical moments, a dream lived by many. It's a rising: invincible, powerful, mystical." />
+    <meta property="og:title" content="Vaayu '20 - Get Empowered!" />
+    <meta property="og:description" content="VAAYU: a metaphor of people, a carrier of hope, a treasure of memories, a haven for magical moments, a dream lived by many. It's a rising: invincible, powerful, mystical." />
+    <title>
+        Vaayu Fest - Happenings
+    </title>
+    <?php include_once('./components/stylesheets.php') ?>
+</head>
+
+<body id="page-top">
+    <?php include_once './components/navigation.php'; ?>
+    <!-- <section id="sponsorsSection" class="customSection">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-1 col-md-1 col-sm-12">
+                    <h2 class="customSectionTitle">
+                        COMMITTEE
+                    </h2>
+                </div>
+                <div class="col-lg-11 col-md-11 col-sm-12">
+                    <img src="media/images/committees/externals.jpg" alt="Committee" style="width: 100%; margin-top: 100px;" />
+                </div>
+            </div>
+            <div class="row">
+            </div>
+        </div>
+    </section> -->
+    <!-- catch the cash - registrations -->
+    <section id="sponsorsSection" class="customSection">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-1 col-md-1 col-sm-12">
+                    <h2 class="customSectionTitle">
+                        IMPETUS
+                    </h2>
+                </div>
+                <div class="col-lg-11 col-md-11 col-sm-12">
+                    <div class="p-5">
+                        <div class="row">
+                            <div class="col-lg-4 col-md-4 p-3">
+                                <img class="img-fluid img-profile mx-auto mb-2 shadow p-3" src="media/impetus/sept-ss.png" alt="" />
+                                <a href="https://vaayufest.org/media/impetus/september-20-1.pdf">
+                                    <h4 class="name text-center text-uppercase">
+                                        ISSUE - September '20
+                                    </h4>
+                                </a>
+                            </div>
+                            <div class="col-lg-4 col-md-4 p-3">
+                                <img class="img-fluid img-profile mx-auto mb-2 shadow p-3" src="media/impetus/oct-ss.png" alt="" />
+                                <a href="https://vaayufest.org/media/impetus/october-20-2.pdf">
+                                    <h4 class="name text-center text-uppercase">
+                                        ISSUE - October '20
+                                    </h4>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+            </div>
+        </div>
+    </section>
+    <?php include_once './components/footer.php'; ?>
+    <?php include_once './components/scripts.php'; ?>
+    <?php include_once './components/modals.php'; ?>
+</body>
+
+</html>
