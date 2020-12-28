72536dd6cd73e814261275feb26d161d9575183a
Gourang Abani <gourangabani@gmail.com>
2020-12-28 15:40:20 +0530

    created 20

---
+++ vaayu-20.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include_once('./components/meta.php') ?>
+    <meta name="description" content="VAAYU: a metaphor of people, a carrier of hope, a treasure of memories, a haven for magical moments, a dream lived by many. It's a rising: invincible, powerful, mystical." />
+    <meta property="og:title" content="Vaayu '20 - Get Empowered!" />
+    <meta property="og:description" content="VAAYU: a metaphor of people, a carrier of hope, a treasure of memories, a haven for magical moments, a dream lived by many. It's a rising: invincible, powerful, mystical." />
+    <title>
+        Vaayu Fest - Vaayu'20
+    </title>
+    <?php include_once('./components/stylesheets.php') ?>
+
+</head>
+
+<body id="page-top">
+    <?php include_once './components/navigation.php'; ?>
+    <!-- registrations -->
+    <section id="vaayu20Section" class="customSection">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-1 col-md-1 col-sm-12">
+                    <h2 class="customSectionTitle">
+                        VAAYU'20
+                    </h2>
+                </div>
+                <div class="col-lg-11 col-md-11 col-sm-12">
+                    <h2>
+                        Contingent Leader's Virtual Meet
+                    </h2>
+                    <div class="row">
+                        <div class="col-md-8 text-center align-items-center">
+                            <iframe width="100%" height="480" src="https://www.youtube-nocookie.com/embed/0Kf__eht2YE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
+                        <div class="col-md-4 text-center align-items-center mb-3 mt-3">
+                            <a class="btn btn-secondary w-100 mb-1 mt-1" href="tel:+919814831300">
+                                <i class="fa fa-phone" aria-hidden="true"></i> Diya Maini (HoD Hospitality)
+                            </a>
+                            <a class="btn btn-secondary w-100 mb-1 mt-1" href="https://www.youtube.com/watch?v=oK1RBl1ohNw&feature=youtu.be">
+                                Date Reveal
+                            </a>
+                            <a class="btn btn-secondary w-100 mb-1 mt-1" href="https://www.youtube.com/watch?v=xeduBMMNP5U&feature=youtu.be">
+                                Vaayu'19 Aftermovie
+                            </a>
+                            <a class="btn btn-secondary w-100 mb-1 mt-1" href="./media/events-brochure.pdf">
+                                Events Brochure
+                            </a>
+                            <a class="btn btn-secondary w-100 mb-1 mt-1" href="/user-registration.php">
+                                User Registration
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+            </div>
+        </div>
+    </section>
+    <?php include_once './components/footer.php'; ?>
+    <?php include_once './components/scripts.php'; ?>
+    <?php include_once './components/modals.php'; ?>
+</body>
+
+</html>
